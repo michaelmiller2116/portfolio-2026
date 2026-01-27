c195a1636e119524175130a781a9632a791cf64f
Mike <michael.miller2116@gmail.com>
2026-01-27 19:28:25 +0100

    initial portfolio commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+
+const Contact = () => {
+  return (
+    <div>ContactPage</div>
+  )
+}
+
+export default Contact
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -64,3 +64,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
